9d551b0ba65bfa238ee9c6c5df0f2e256d92cbed
Andrea Giuliano <andregiu3200@gmail.com>
2017-03-29 10:48:19 +0200

    Rejected commit: Default

---
+++ app/src/main/java/it/polito/group05/group05/Utility/Expense.java
@@ -0,0 +1,60 @@
+package it.polito.group05.group05.Utility;
+
+import android.widget.ImageView;
+
+/**
+ * Created by Andrea on 25-Mar-17.
+ */
+
+public class Expense {
+
+    private String name;
+    private String description;
+    private String owner;
+    private double amount;
+    //private String amount;
+    private ImageView e_image;
+    //File
+    //Deadline
+    public Expense(String expenseName, double expenseAmount, String expenseOwner, ImageView expenseImage )
+    {
+        this.e_image = expenseImage;
+        this.name = expenseName;
+        this.amount= expenseAmount;
+        this.owner = expenseOwner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ImageView getE_image() {
+        return e_image;
+    }
+
+    public void setE_image(ImageView e_image) { this.e_image = e_image; }
+
+    public String getOwner() { return owner; }
+
+    public void setOwner(String owner) { this.owner = owner; }
+}
