8b25472fd3efc95a143d421fc2384a8e1ca40861
annachiara19 <ruospoannachiara@gmail.com>
2017-04-03 23:00:55 +0200

    PRIMA DI DIRE PAROLACCE, SALVIAMO

--- app/src/main/java/it/polito/group05/group05/ExpenseActivity.java
+++ app/src/main/java/it/polito/group05/group05/ExpenseActivity.java
@@ -3,49 +3,53 @@ package it.polito.group05.group05;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 import it.polito.group05.group05.R;
 
+import static android.widget.AdapterView.*;
+
 public class ExpenseActivity extends AppCompatActivity {
 
     private MaterialEditText et_name, et_description, et_cost, et_file;
     private CheckBox cb_description, cb_addfile, cb_adddeadline, cb_proposal;
-    private ImageView im;
-    private Spinner spinner;
+    private TextView tv_policy;
+    private Spinner spinner, spinner_policy;
     private Button addExpense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense_v2);
-
-        im= (ImageView) findViewById(R.id.image1);
-        et_name= (MaterialEditText) findViewById(R.id.et_name_expense);
-        et_cost= (MaterialEditText) findViewById(R.id.et_cost_expense);
-        cb_description= (CheckBox) findViewById(R.id.cb1_description);
+        et_name = (MaterialEditText) findViewById(R.id.et_name_expense);
+        et_cost = (MaterialEditText) findViewById(R.id.et_cost_expense);
+        cb_description = (CheckBox) findViewById(R.id.cb1_description);
         // hide until its title is clicked
-        et_description= (MaterialEditText) findViewById(R.id.et_description_expense);
+        et_description = (MaterialEditText) findViewById(R.id.et_description_expense);
         et_description.setVisibility(View.GONE);
-
-
-        cb_addfile= (CheckBox) findViewById(R.id.cb2_addfile);
-        cb_adddeadline= (CheckBox) findViewById(R.id.cb3_deadline);
+        cb_addfile = (CheckBox) findViewById(R.id.cb2_addfile);
         // hide until its title is clicked
-        et_file= (MaterialEditText) findViewById(R.id.et_addfile_expense);
+        et_file = (MaterialEditText) findViewById(R.id.et_addfile_expense);
         et_file.setVisibility(View.GONE);
-
-        spinner= (Spinner) findViewById(R.id.spinner);
+        cb_adddeadline = (CheckBox) findViewById(R.id.cb3_deadline);
+        spinner = (Spinner) findViewById(R.id.spinner);
         spinner.setVisibility(View.GONE);
+        cb_proposal = (CheckBox) findViewById(R.id.cb4_proposal);
+        tv_policy= (TextView) findViewById(R.id.tv_policy);
+        spinner_policy = (Spinner) findViewById(R.id.spinner_policy);
 
-        cb_proposal= (CheckBox) findViewById(R.id.cb4_proposal);
-        addExpense= (Button) findViewById(R.id.buttonfinal);
     }
+
     public void description_handler(View v){
         et_description.setVisibility(et_description.isShown() ? View.GONE : View.VISIBLE);
     }
