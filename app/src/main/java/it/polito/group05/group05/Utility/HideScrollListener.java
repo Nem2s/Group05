fbc965eee0eb15fd00ebc2e991804f9c6c31c1ff
wargas92 <nino.rocca92@gmail.com>
2017-04-06 18:58:12 +0200

    dffdfd

---
+++ app/src/main/java/it/polito/group05/group05/Utility/HideScrollListener.java
@@ -0,0 +1,46 @@
+package it.polito.group05.group05.Utility;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Marco on 05/04/2017.
+ */
+
+public abstract class HideScrollListener extends RecyclerView.OnScrollListener {
+    private static final int HIDE_THRESHOLD = 10;
+    private int scrolledDistance = 0;
+    private boolean controlsVisible = true;
+
+    @Override
+    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+
+        int firstVisibleItem = ((LinearLayoutManager) recyclerView.getLayoutManager()).findFirstVisibleItemPosition();
+        //show views if first item is first visible position and views are hidden
+        if (firstVisibleItem == 0) {
+            if(!controlsVisible) {
+                onShow();
+                controlsVisible = true;
+            }
+        } else {
+            if (scrolledDistance > HIDE_THRESHOLD && controlsVisible) {
+                onHide();
+                controlsVisible = false;
+                scrolledDistance = 0;
+            } else if (scrolledDistance < -HIDE_THRESHOLD && !controlsVisible) {
+                onShow();
+                controlsVisible = true;
+                scrolledDistance = 0;
+            }
+        }
+
+        if((controlsVisible && dy>0) || (!controlsVisible && dy<0)) {
+            scrolledDistance += dy;
+        }
+    }
+
+    public abstract void onHide();
+    public abstract void onShow();
+
+}
