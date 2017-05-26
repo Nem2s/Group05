6edd2c01ea41ae4b655c4085e84ce1bc01f20d06
wargas92 <nino.rocca92@gmail.com>
2017-05-26 14:43:57 +0200

    Merge branches 'Nino' and 'anna' of https://github.com/Nem2s/Group05 into Nino
    # Conflicts:
    #	app/src/main/java/it/polito/group05/group05/Expense_activity.java
    #	app/src/main/java/it/polito/group05/group05/Utility/Holder/ExpenseHolder.java
    #	app/src/main/res/layout/chat_main.xml

--- app/src/main/java/it/polito/group05/group05/ExpenseFragment.java
+++ app/src/main/java/it/polito/group05/group05/ExpenseFragment.java
@@ -198,7 +198,7 @@ public class ExpenseFragment extends Fragment {
             @Override
             protected void populateViewHolder(ExpenseHolder viewHolder, ExpenseDatabase model, int position) {
                 if (model == null) return;
-                viewHolder.setData(model, getContext(), ei);
+                viewHolder.setData(model, getContext());
 
             }
         };
